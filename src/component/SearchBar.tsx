a3ce371ee227e1251e8e17ecb465616fb23b0258
yl544350359 <yanli1030@outlook.com>
2023-01-28 21:46:23 +0900

    display result

--- src/component/SearchBar.tsx
+++ src/component/SearchBar.tsx
@@ -1,15 +1,41 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
-import Divider from '@mui/material/Divider';
 import { useTranslation } from "react-i18next";
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import HighlightOffTwoToneIcon from '@mui/icons-material/HighlightOffTwoTone';
+import {ItemDetail} from '../page/Calculator'
 
-export default function SearchBar() {
+type Props= {
+  item: ItemDetail|null;
+  setItem: (arg0: ItemDetail) => void;
+  setErrmsg: (arg0: string) => void;
+  setLoading :(arg0: boolean) => void
+}
+
+export default function SearchBar({item, setItem, setErrmsg, setLoading}:Props) {
     const [url, setUrl] = React.useState<string>("");
     const { t } = useTranslation();
+    const handleSearch=(itemUrl:string) => {
+      setErrmsg("");
+      setLoading(true);
+      fetch("http://localhost:8001/itemDetail", {
+        method: 'POST',
+        body: JSON.stringify({
+          item_url: itemUrl
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8'
+        }
+      }).then((response)=> response.json())
+      .then((data) => {
+        console.log(data);
+        setLoading(false);
+        setItem(data);
+      })
+      .catch((err) => setErrmsg(err.message))
+    }
     return (
         <Paper
         component="form"
@@ -25,13 +51,9 @@ export default function SearchBar() {
       {url && <IconButton type="button" sx={{ p: '10px' }} aria-label="delete" onClick={() => setUrl("")}>
         <HighlightOffTwoToneIcon />
       </IconButton>}
-      <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+      <IconButton type="button" sx={{ p: '10px' }} onClick={() => handleSearch(url)} aria-label="search">
         <SearchIcon />
       </IconButton>
-      {/* <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
-        <DirectionsIcon />
-      </IconButton> */}
     </Paper>
   );
 }
--- src/page/Calculator.tsx
+++ src/page/Calculator.tsx
@@ -1,16 +1,88 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import SearchBar from '../component/SearchBar'
-export default function Calculator(){
-    return(
-        <Box 
-        sx={{
-            display: "flex",
-            width: "100%",
-            mt:1,
-            justifyContent:"center"
-        }}>
-            <SearchBar/>
+import Alert from '@mui/material/Alert';
+import SearchBar from '../component/SearchBar';
+import { AlertTitle, 
+    CircularProgress, 
+    Card, 
+    CardContent,
+    CardMedia,
+    Typography,
+    Chip
+ } from '@mui/material';
+ import CheckIcon from '@mui/icons-material/Check';
+ import Divider from '@mui/material/Divider';
+ import ClearIcon from '@mui/icons-material/Clear';
+ import { useTranslation } from "react-i18next";
+
+export interface ItemDetail {
+    price_jpy: number;
+    price_cny: number;
+    item_name: string;
+    img_url: string;
+    shipping_fee_tag: Boolean;
+    sold_out_flag: Boolean;
+    discription: string
+}
+
+export default function Calculator() {
+    const [item, setItem] = React.useState<ItemDetail | null>(null)
+    const [errmsg, setErrmsg] = React.useState<string>("")
+    const [loading, setLoading] = React.useState<boolean>(false)
+    const { t } = useTranslation();
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                width: "100%",
+                height: "100%",
+                mt: 1,
+                alignItems: "center",
+                gap: 1
+            }}>
+            <SearchBar item={item} setItem={setItem} setErrmsg={setErrmsg} setLoading={setLoading} />
+            {item  && !errmsg && !loading && <Card sx={{ maxWidth: 400}}>
+                <CardMedia
+                component='img'
+                alt='This is picture'
+                height="350"
+                image={item.img_url}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5">
+                    {item.item_name}
+                    </Typography>
+                    <Typography sx={{color:'#1976d2'}} variant="h4">
+                        {`${t("calculator.cny_price")}${item.price_cny}`}
+                    </Typography>
+                    <Typography sx={{color:'#bdbdbd'}}>
+                        {`${t("calculator.jpy_price")}${item.price_jpy}`}
+                    </Typography>
+                    <Box sx={{
+                        display:"flex",
+                        gap:1,
+                        justifyContent:"center"
+                    }}
+                    margin={1}>
+                        {item.shipping_fee_tag? <Chip icon={<CheckIcon />} color="success" label={t("calculator.include_fee_label")} />:<Chip icon={<ClearIcon />} color="error" label={t("calculator.exclude_fee_lable")} />}
+                        {item.sold_out_flag? <Chip icon={<ClearIcon />} color="error" label={t("calcualtor.soldout_lable")} />:<Chip icon={<CheckIcon />} color="success" label={t("calculator.buyable_label")} />}
+                    </Box>
+                    <Divider />
+                    <Typography>
+                    {item.discription}
+                    </Typography>
+                </CardContent>
+            </Card>}
+            {loading && <CircularProgress />}
+            {errmsg && <Alert
+                severity='error'
+                sx={{
+                    width: 400
+                }}>
+                <AlertTitle>Error</AlertTitle>
+                {errmsg}
+            </Alert>}
         </Box>
     )
 }
