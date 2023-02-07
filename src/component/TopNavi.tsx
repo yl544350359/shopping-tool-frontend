00c98a976a568fdad8145835850fd5d94a7a6ea9
yl544350359 <yanli1030@outlook.com>
2023-02-07 15:45:58 +0900

    set the correct title when open website

--- src/component/TopNavi.tsx
+++ src/component/TopNavi.tsx
@@ -3,14 +3,30 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-// import MenuIcon from '@mui/icons-material/Menu';
 import PageMenu from './Menu'
 import { useTranslation } from "react-i18next";
+import { useLocation } from 'react-router-dom';
 
 export default function TopNavigator() {
   const { t } = useTranslation();
-  const [title, setTitle] = React.useState(t("common.calculator")!);
+  const location=useLocation();
+  var tmp:string;
+  if(location.pathname==="/"){
+    tmp=t("common.calculator");
+  }
+  else if(location.pathname==="/Calculator"){
+    tmp=t("common.calculator");
+  }
+  else if(location.pathname==="/ShippingPrice"){
+    tmp=t("common.shipping_price");
+  }
+  else {
+    tmp=t("common.not_found");
+  }
 
+
+  const [title,setTitle] = React.useState(tmp);
+ 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
