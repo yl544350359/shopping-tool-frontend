9f4ed72ea8d20efb8a68e75246a298510f3d7fe2
LI YAN <48004652+yl544350359@users.noreply.github.com>
2023-10-13 20:28:50 +0900

    update myhome page

--- src/page/MyHome.tsx
+++ src/page/MyHome.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { useTranslation } from "react-i18next";
 
 const bull = (
     <Box
@@ -15,42 +18,66 @@ const bull = (
     </Box>
   );
   
-  const card = (
-    <React.Fragment>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
-        <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </React.Fragment>
-  );
 
 export default function MyHome(){
-    return(
-        <Box sx={{
-            display: "flex",
-            flexDirection: "column",
-            width: "100%",
-            height: "100%",
-            mt: 1,
-            alignItems: "center",
-            gap: 1
-        }}>
-      <Card variant="outlined" sx={{ maxWidth: 400 }}>{card}</Card>
-    </Box>
-    )
+  const { t } = useTranslation();
+  return(
+      <Box sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          height: "100%",
+          mt: 1,
+          alignItems: "center",
+          gap: 1
+      }}>
+    <Card variant="outlined" sx={{ maxWidth: 600}}>
+      <CardMedia
+      sx={{
+        height: '200px'
+      }}
+        image="/background.jpeg"
+        title="background"
+      />
+      <CardContent sx={{display: "flex", 
+      justifyContent: "center", 
+      flexDirection: "column"}}>
+      <Box sx={{
+        position: "fixed",
+        top: "252px",
+        display: "flex",
+        gap: 2,
+        flexDirection: "column",
+        alignSelf: "center",
+        alignItems: "center"
+      }}>
+        <Avatar 
+          alt="avatar" 
+          src="/avatar-profile.svg" 
+          sx={{
+            backgroundColor: "white",
+            width: 56, 
+            height: 56,
+          }}
+        />
+        <Typography 
+          gutterBottom 
+          variant="h5" 
+          component="div"
+        >
+          {t('my_home.username')}
+        </Typography>
+      </Box>
+      <Typography variant="body2" color="text.secondary" sx={{marginTop: '56px'}}>
+        Lizards are a widespread group of squamate reptiles, with over 6,000
+        species, ranging across all continents except Antarctica
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Share</Button>
+      <Button size="small">Learn More</Button>
+    </CardActions>
+    </Card>
+  </Box>
+  )
 }
