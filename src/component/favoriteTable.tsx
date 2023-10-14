6d7ee42e45ff25cc2db94ebe626fccf96dc80232
yl544350359 <yanli1030@outlook.com>
2023-10-14 21:49:55 +0900

    add favorite table

---
+++ src/component/favoriteTable.tsx
@@ -0,0 +1,174 @@
+import * as React from 'react';
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { visuallyHidden } from '@mui/utils';
+import { ItemDetail } from '../page/Calculator';
+import { useTranslation } from "react-i18next";
+
+type Props={
+    items: ItemDetail[],
+    selected: string[],
+    setSelected: (arg0: string[]) => void;
+};
+
+interface Column {
+    id: 'check_box' | 'image_url' | 'item_name' | 'price_cny';
+    label: string;
+    minWidth?: number;
+    align?: 'right' | 'center';
+}
+
+export default function FavoriteTable({items, selected, setSelected}:Props) {
+    const { t } = useTranslation();
+    const isSelected = (name: string) => selected.indexOf(name) !== -1;
+    const headCells: readonly Column[] = [
+        {
+            id: 'check_box',
+            label: t("my_home.select"),
+            minWidth: 48,
+            align: 'center'
+        },
+        {
+            id: 'image_url',
+            label: t("my_home.picture"),
+            minWidth: 48,
+            align:'center'
+        },
+        {
+            id: 'item_name',
+            label: t("my_home.title"),
+            minWidth: 96,
+            align: 'center'
+        },
+        {
+            id: 'price_cny',
+            label: t("my_home.price"),
+            minWidth: 60,
+            align: 'center'
+        }
+    ];
+    const handleSelect = (event: React.MouseEvent<unknown>, name: string) => {
+        const selectedIndex = selected.indexOf(name);
+        let newSelected: string[] = [];
+    
+        if (selectedIndex === -1) {
+          newSelected = newSelected.concat(selected, name);
+        } else if (selectedIndex === 0) {
+          newSelected = newSelected.concat(selected.slice(1));
+        } else if (selectedIndex === selected.length - 1) {
+          newSelected = newSelected.concat(selected.slice(0, -1));
+        } else if (selectedIndex > 0) {
+          newSelected = newSelected.concat(
+            selected.slice(0, selectedIndex),
+            selected.slice(selectedIndex + 1),
+          );
+        }
+        setSelected(newSelected);
+      };
+
+      const handleSelectAll = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.checked) {
+          const newSelected = items.map((n) => n.item_name);
+          setSelected(newSelected);
+          return;
+        }
+        setSelected([]);
+      };
+
+    return (
+        <Paper sx={{ width: '100%' }}>
+            <TableContainer>
+                <Table
+                    // sx={{ minWidth: 600 }}
+                    aria-labelledby="tableTitle"
+                    size='medium'
+                >
+                    <TableHead>
+                    <TableRow>
+                        {headCells.map((column, index) => (
+                            <TableCell
+                                key={column.id}
+                                align={column.align}
+                                style={{
+                                    minWidth: column.minWidth,
+                                    position: index === 0 ? "sticky" : undefined,
+                                    left: index === 0 ? 0 : undefined,
+                                    zIndex: index === 0 ? 99 : 0,
+                                    top: '-1px'
+                                }}
+                            >
+                            {index === 0 ? <Checkbox
+                                        color="primary"
+                                        indeterminate={selected.length > 0 && selected.length < items.length}
+                                        checked={items.length > 0 && items.length === selected.length}
+                                        onChange={handleSelectAll}
+                                        inputProps={{
+                                        'aria-label': 'select all items',
+                                        }}
+                                    /> : column.label}
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {items.map((row, index) => {
+                            const isItemSelected = isSelected(row.item_name);
+                            const labelId = `enhanced-table-checkbox-${index}`;
+
+                            return (
+                                <TableRow
+                                    hover
+                                    onClick={(event) => handleSelect(event, row.item_name)}
+                                    role="checkbox"
+                                    aria-checked={isItemSelected}
+                                    tabIndex={-1}
+                                    key={row.name}
+                                    selected={isItemSelected}
+                                    sx={{ cursor: 'pointer' }}
+                                >
+                                    <TableCell align="center" padding="checkbox">
+                                        <Checkbox
+                                            color="primary"
+                                            checked={isItemSelected}
+                                            inputProps={{
+                                                'aria-labelledby': labelId,
+                                            }}
+                                        />
+                                    </TableCell>
+                                    <TableCell align="left"><img src={row.img_url} alt={row.item_name} width="50" /></TableCell>
+                                    <TableCell
+                                        component="th"
+                                        id={labelId}
+                                        scope="row"
+                                        padding="none"
+                                    >
+                                        {row.item_name}
+                                    </TableCell>
+                                    <TableCell align="center">{row.price_cny}</TableCell>
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Paper>
+    );
+}
--- src/page/MyHome.tsx
+++ src/page/MyHome.tsx
@@ -12,19 +12,72 @@ import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import FavoriteTable from '../component/favoriteTable';
+import Toolbar from '@mui/material/Toolbar';
+import { alpha } from '@mui/material/styles';
+import Tooltip from '@mui/material/Tooltip';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import { ItemDetail } from './Calculator';
 
-const bull = (
-    <Box
-        component="span"
-        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-    >
-        •
-    </Box>
-);
+interface EnhancedTableToolbarProps {
+    numSelected: number;
+  }
+  
+function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
+    const { numSelected } = props;
+  
+    return (
+      <Toolbar
+        sx={{
+        justifyContent: "space-between",
+        width: "100%",
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        minHeight: {sm: 24 }
+        //   ...(numSelected > 0 && {
+        //     bgcolor: (theme) =>
+        //       alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
+        //   }),
+        }}
+      >
+        {numSelected > 0 && (
+          <Typography
+            sx={{ flex: '1 1 100%' }}
+            color="inherit"
+            variant="subtitle1"
+            component="div"
+          >
+            {numSelected} selected
+          </Typography>
+        ) }
+        {numSelected > 0 && (
+          <Tooltip title="Delete">
+            <IconButton>
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+        )}
+      </Toolbar>
+    );
+  }
 
+const staticData:ItemDetail[]=[{
+    "item_url": "https://jp.mercari.com/item/m61443717755",
+    "price_jpy": 750,
+    "price_cny": 49,
+    "item_name": "新品未使用品 GU ニット トップス レディース ブルー系",
+    "img_url": "https://static.mercdn.net/item/detail/orig/photos/m61443717755_1.jpg?1695801275",
+    "shipping_fee_tag": true,
+    "sold_out_flag": false,
+    "discription": "数年前にGUで購入しました。\nタグ付いてませんが一度も着てません。\n\n家保管ですが綺麗でいい状態です！\nLサイズ。\n\n何かあれば、ご相談ください！"
+}]
 
 export default function MyHome() {
     const { t } = useTranslation();
+    const [selected, setSelected] = React.useState<string[]>([]);
+    const [items, setItems] = React.useState<ItemDetail[]>(staticData);
+
     return (
         <Box sx={{
             display: "flex",
@@ -72,22 +125,20 @@ export default function MyHome() {
                         >
                             {t('my_home.username')}
                         </Typography>
+                        <Accordion sx={{maxWidth:'600px'}}>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
+                            >
+                                <Typography sx={{display: "flex", minWidth: '60px', alignItems: "center"}}>{t("my_home.favorite")}</Typography>
+                                <EnhancedTableToolbar numSelected={selected.length}/>
+                            </AccordionSummary>
+                            <AccordionDetails sx={{padding: 0}}>
+                                <FavoriteTable items={items} selected={selected} setSelected={setSelected}/>
+                            </AccordionDetails>
+                        </Accordion>
                     </Box>
-                    <Accordion>
-                        <AccordionSummary
-                            expandIcon={<ExpandMoreIcon />}
-                            aria-controls="panel1a-content"
-                            id="panel1a-header"
-                        >
-                            <Typography>Accordion 1</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <Typography>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                malesuada lacus ex, sit amet blandit leo lobortis eget.
-                            </Typography>
-                        </AccordionDetails>
-                    </Accordion>
                 </CardContent>
             </Card>
         </Box>
